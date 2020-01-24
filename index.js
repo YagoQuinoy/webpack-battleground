364cead41e722eb9f00789d5ac0d76cfd1ff1add
Yago Quiñoy Lobariñas <yagoquinoy@gmail.com>
2020-01-24 16:37:20 +0100

    leave only webpack html plugin bis

--- index.js
+++ index.js
@@ -1,18 +1,6 @@
 const express = require('express');
-// const exphbs = require('express-handlebars');
-
-// const manifest = require('./public/manifest.json');
 
 const app = express();
-
-// app.engine('handlebars', exphbs());
-// app.set('view engine', 'handlebars');
-// app.get('/', (req, res) => {
-//   const mapped = Object.keys(manifest).map((key) => manifest[key]);
-//   res.render('index', {
-//     manifest: mapped,
-//   });
-// });
 
 app.use(express.static('public'));
 
